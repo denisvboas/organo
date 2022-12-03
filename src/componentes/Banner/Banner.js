63506245a4141894bc415bf6f75f38eceebc5e59
denisvboas <denisssvilasboas@gmail.com>
2022-12-02 21:51:58 -0300

    Projeto organo

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+import "./Banner.css";
+
+export const Banner = () => {
+  // JSX
+  return (
+    <Fragment>
+      <header className="banner">
+        <img src="/imagens/banner.png" alt="O banner principal do organo" />
+      </header>
+    </Fragment>
+  );
+};
